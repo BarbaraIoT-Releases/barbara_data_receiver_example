f665ea6fa24b3f9942cf0d799b46c440de4c20cc
J Mir <jmir.17@gmail.com>
2018-10-26 16:04:08 +0200

    Add mongodb connections

--- src/server.js
+++ src/server.js
@@ -13,6 +13,24 @@ if (!HOST) {
     process.exit();
 }
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/barbaraIoT';
+const dbName = 'barbaraIoT';
+
+const mongoClient = new MongoClient(url);
+
+mongoClient.connect((err) => {
+    if (err) {console.error(err)}
+    console.log("Connected successfully to MongoDB server");
+});
+
+const insertDocument = (document2Insert) => {
+    db.collection(TOPOC).insert(document2Insert, (err, result) => {
+        if (err) {console.error(err)}
+        console.log("Document inserted into the mongoDB collection");
+    });
+}
+
 options = {
     port: 8883,
     host: HOST,
@@ -33,5 +51,6 @@ mqttClient.on('message',(topic, message)=>{
     if (topic.startsWith(TOPIC+'/')){
         const deviceId = topic.replace(TOPIC+'/','');
         console.log('Received message from device: '+ deviceId + ' \nMessage: ' + message + '\n');
+        insertDocument(message);
     }
 });
