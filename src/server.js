a35866a0774d895c61b6ba82cfe64554f78ddf0c
J Mir <jmir.17@gmail.com>
2018-10-29 10:58:16 +0100

    change mongo port

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 //MongoDB connection data
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://172.16.50.10:37017';
+const url = 'mongodb://172.16.50.10:27017';
 const dbName = 'barbaraIoT';
-const mongoClient = new MongoClient(url);
+const mongoClient = new MongoClient(url, {useNewUrlParser: true, reconnectTries: 60, reconnectInterval: 1000});
 
 //MQTT required information
 const fs = require('fs');
@@ -21,12 +21,13 @@ if (!HOST) {
 
 
 mongoClient.connect((err) => {
-    if (err) {console.error(err)}
-    console.log('Connected successfully to MongoDB server');
+    if (err) {console.error(err)} else {
+        console.log('Connected successfully to MongoDB server');
+    }
 });
 
 const insertOneDocument = (db, document2Insert) => {
-    db.collection(TOPIC).insertOne(JSON.parse(document2Insert), (err, result) => {
+    db.collection(TOPIC).insertOne(document2Insert, (err, result) => {
         if (err) {console.error(err)}
         console.log('Inserted into the mongoDB collection. \n');
     });
@@ -55,6 +56,8 @@ mqttClient.on('message',(topic, message)=>{
     if (topic.startsWith(TOPIC+'/')){
         const deviceId = topic.replace(TOPIC+'/','');
         console.log('Received message from device: '+ deviceId + ' \nMessage: ' + message);
-        insertOneDocument(mongoClient.db(dbName), message);
+        document2Insert = JSON.parse(message.toString().replace('\n',''));
+        document2Insert.timestamp = new Date();
+        insertOneDocument(mongoClient.db(dbName), document2Insert);
     }
 });
